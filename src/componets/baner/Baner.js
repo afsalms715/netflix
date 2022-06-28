6eadc46d49364b248c7ee911c3a60ba01b80a000
afsalms715 <afsalms715@gamil.com>
2022-06-28 22:27:13 +0300

    banner image, title and description in dinamicaly
    Signed-off-by: afsalms715 <afsalms715@gamil.com>

--- src/componets/baner/Baner.js
+++ src/componets/baner/Baner.js
@@ -1,19 +1,27 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
+import { APIKEY,imageUrl} from '../../constance/constance';
 import './Baner.css'
+import axios from '../axios'
 function Baner() {
+  const [movie, setMovie] = useState();
   useEffect(() => {
-    
+    axios.get(`trending/all/week?api_key=${APIKEY}&language=en-US`).then((responce)=>{
+      console.log(responce.data.results[0])
+      setMovie(responce.data.results[0])
+    })
     
   }, []);
   return (
-    <div className='baner'>
+    <div
+    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path:""})`}}
+     className='baner'>
         <div className='content'>
-            <h1 className='title'>Title</h1>
+            <h1 className='title'>{movie ? movie.title:""}</h1>
             <div className='content-buttons'>
                 <button className='buttons'>play</button>
                 <button className='buttons'>My list</button>
             </div>
-            <h1 className='discription'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without</h1>
+            <h1 className='discription'>{movie ? movie.overview:""}</h1>
         </div>
         <div className="fade-bottom">
         </div>
---
+++ src/constance/constance.js
@@ -0,0 +1,3 @@
+export const baseUrl="https://api.themoviedb.org/3/";
+export const APIKEY="8cf92b2e95ee29f234a90cee51f4539d";
+export const imageUrl="https://image.tmdb.org/t/p/original/";
